6f5d836cc19dff2d6026007766d1ae690a8fc74f
Your Name <you@example.com>
2022-02-28 21:16:28 -0800

    added tooltips to bubble chart and multiple lines for line chart

--- src/bubblechart/bubblechart.js
+++ src/bubblechart/bubblechart.js
@@ -1,4 +1,4 @@
-function main(){
+function main() {
     //credits to
     /* Alark Joshi's Skeleton Code
     * https://www.d3-graph-gallery.com/graph/bubble_basic.html
@@ -9,105 +9,99 @@ function main(){
 
     const svg = d3.select("body").append("svg")
         .attr("width", canvasWidth)
-        .attr("height",canvasHeight)
+        .attr("height", canvasHeight)
 
     const width = svg.attr("width") - margin;
     const height = svg.attr("height") - margin;
 
     //add text to the canvas for the title
     svg.append("text")
-        .attr("transform","translate(100,0)")
-        .attr("x",50)
-        .attr("y",50)
-        .attr("font-size","20px")
-        .attr("font-family","sans-serif")
-        .text("Bubble Chart for Car Dimension and Car HorsePower/Torque")
+        .attr("transform", "translate(100,0)")
+        .attr("x", 50)
+        .attr("y", 50)
+        .attr("font-size", "20px")
+        .attr("font-family", "sans-serif")
+        .text("Bubble Chart for Different Plays and their popularity based on Sales, Theatres Filled, and Attendance")
 
 
     //create the scales for the x y and size.
-    let xScale = d3.scaleLinear().range([0,width]);
-    let yScale = d3.scaleLinear().range([height,0]);
-    let sizeScale = d3.scaleLinear().range([2,10]);
+    let xScale = d3.scaleLinear().range([0, width]);
+    let yScale = d3.scaleLinear().range([height, 0]);
+    let sizeScale = d3.scaleLinear().range([2, 10]);
 
 
     const container_g = svg.append("g")
         .attr("transform",
             "translate(100,100)");
 
-    d3.csv("https://gist.githubusercontent.com/tlin41390/e7aebdfef38808e0d362143ce7a8b8ce/raw/98256dbdd4b8b1e91a2c94ec5b4b61f5144ca159/broadway.csv").then(data=>{
-        //set the domain
-        // container_g.append("text")
-        //     .attr("id", "tooltip")
-        //     // .attr("x")
-        xScale.domain([1500,15000]);
-        yScale.domain([0,100]);
+    d3.csv("https://gist.githubusercontent.com/tlin41390/e7aebdfef38808e0d362143ce7a8b8ce/raw/98256dbdd4b8b1e91a2c94ec5b4b61f5144ca159/broadway.csv").then(data => {
+        xScale.domain([1500, 15000]);
+        yScale.domain([0, 100]);
 
-        sizeScale.domain([40000,900000]);
+        sizeScale.domain([40000, 900000]);
 
         //append the bubbles to the chart
         container_g.selectAll(".dot")
             .data(data)
             .enter().append("circle")
-            .attr("class","dot")
-            .attr("cx",function(d){ return xScale(d.Attendance); })
-            .attr("cy",function(d){ return yScale(d.Capacity);})
-            .attr("r",function(d){ return sizeScale(d.Gross);})
+            .attr("class", "dot")
+            .attr("cx", function (d) {
+                return xScale(d.Attendance);
+            })
+            .attr("cy", function (d) {
+                return yScale(d.Capacity);
+            })
+            .attr("r", function (d) {
+                return sizeScale(d.Gross);
+            })
             //set the categorical variables for the color.
-            .attr("fill",function(d){
-                if(d.Type == "Play"){
+            .attr("fill", function (d) {
+                if (d.Type == "Play") {
                     return "dodgerblue"
-                }else if(d.Type =="Musical"){
+                } else if (d.Type == "Musical") {
                     return "limegreen";
-                }else{
+                } else {
                     return "crimson";
                 }
             })
-            .on("mouseover",function(d){
-                let xPosition = parseFloat(d3.select(this).attr("cx"));
-                let yPosition = parseFloat(d3.select(this).attr("cy"));
+            .on("mouseover", function (Event, d) {
                 d3.select(this)
-                    .attr("stroke","black")
-                    .append("text")
-                    .attr("id","tooltip")
-                    .attr("x",xPosition)
-                    .attr("y",yPosition)
-                    .attr("text-anchor","middle").attr("font-family", "sans-serif")
-                    .attr("font-size", "11px")
-                    .attr("font-weight", "bold")
-                    .attr("fill", "black")
-                    .text(d);
+                    .attr("stroke", "black")
             })
-            .on("mouseout",function(){
+            .on("mouseout", function () {
                 d3.select(this)
-                    .attr("stroke","none");
-                d3.select("#tooltip").remove();
+                    .attr("stroke", "none");
             })
-
+            .append("title")
+            .text(function (d) {
+                return "(Type: " + d.Type + ") (Name: " + d["Show.Name"] + ") (Earnings: " + " $" + d.Gross + ") (Theatre: " + d["Show.Theatre"] + ") (Capacity: " + d.Capacity + "%" + ") (Attendance: " + d.Attendance + ")";
+            })
         //append the x axis
         container_g.append("g")
             .attr("transform", "translate(0, " + height + ")")
             .call(d3.axisBottom(xScale))
             .append("text")
-            .attr("y", height-450)
-            .attr("x",width-250)
-            .attr("font-size","30px")
+            .attr("y", height - 450)
+            .attr("x", width - 250)
+            .attr("font-size", "30px")
             .attr("stroke", "black")
-            .attr("font-family","sans-serif")
+            .attr("font-family", "sans-serif")
             .text("Total Attendance of That show");
 
         //append the y axis
         container_g.append("g")
             .call(d3.axisLeft(yScale).ticks(15))
             .append("text")
-            .attr("font-size","25px")
-            .attr("transform","rotate(-90)")
-            .attr("y",40)
-            .attr("x",-100)
-            .attr("dy","-4.1em")
-            .attr("stroke","black")
+            .attr("font-size", "25px")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 40)
+            .attr("x", -100)
+            .attr("dy", "-4.1em")
+            .attr("stroke", "black")
             .text("Percentage of Theatre that was filled");
 
 
     })
 }
+
 main();
--- src/linechart/linechart.js
+++ src/linechart/linechart.js
@@ -3,9 +3,9 @@ function main(){
     /* Alark Joshi's Skeletion Code
     /* https://www.d3-graph-gallery.com/graph/line_basic.html
     */
-    const canvasWidth = 700;
-    const canvasHeight = 700;
-    const margin = 200;
+    const canvasWidth = 800;
+    const canvasHeight = 800;
+    const margin = 400;
 
     const svg = d3.select("body").append("svg")
         .attr("width", canvasWidth)
@@ -21,7 +21,7 @@ function main(){
         .attr("y", 50)
         .attr("font-size","20px")
         .attr("font-family","sans-serif")
-        .text("LineChart for Total Sales on Miss Saigon");
+        .text("LineChart for Shows in 1991-1994");
 
 
     const yScale = d3.scaleLinear().range([height,0]);
@@ -30,19 +30,22 @@ function main(){
         .attr("transform","translate(100,100)");
 
     //convert the string to time format
-    d3.csv("https://gist.githubusercontent.com/tlin41390/6733ca28a6f864be20d7bcfb4028d325/raw/9564327eb2892a01c335ee7e993b1c1ef607a3de/broadway_saigon.csv", function(d){ return {date:d3.timeParse("%m/%d/%y")(d.Date), value:d.Gross}}).then(data=>{
+    d3.csv("https://gist.githubusercontent.com/tlin41390/e7aebdfef38808e0d362143ce7a8b8ce/raw/24a3ab225e4ee1208bbe669e1c7af97ef340e98e/broadway.csv", function(d){ return {date:d3.timeParse("%m/%d/%y")(d.Date), value:d.Gross, name:d.Name}}).then(data=>{
         const xScale = d3.scaleTime()
             .domain(d3.extent(data, function(d) { return d.date}))
             .range([0,width]);
 
         yScale.domain([0,d3.max(data, d=>d.value)])
 
+        let sumstat = d3.group(data,d=>d.name)
+        console.log(sumstat);
+
         //append time as the x axis
         container_g.append("g")
             .attr("transform","translate(0," + height + ")")
             .call(d3.axisBottom(xScale).ticks(d3.timeYear))
             .append("text")
-            .attr("y",height - 450)
+            .attr("y",height - 350)
             .attr("x", width-250)
             .attr("font-size","30px")
             .attr("stroke","black")
@@ -55,23 +58,47 @@ function main(){
             .attr("font-size","25px")
             .attr("transform","rotate(-90)")
             .attr("y",40)
-            .attr("x",-100)
+            .attr("x",-80)
             .attr("dy","-4.1em")
             .attr("stroke","black")
             .text("Earnings for the Show");
 
+        //color scale for the different plays that are being represented.
+        const color = d3.scaleOrdinal()
+            .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#1e90ff'])
+
+
         //create the line for the visual
-        container_g.append("path")
-            .datum(data)
+        container_g.selectAll(".line")
+            .data(sumstat)
+            .join("path")
             .attr("fill","none")
-            .attr("stroke","blue")
+            .attr("stroke",function(d){ return color(d[0])})
             .attr("stroke-width",1.5)
             //use the built in d3 line function to create the line for the visual
-            .attr("d",d3.line()
-                //chart the x and y axis based on variables.
-                .x(d => xScale(d.date))
-                .y(d => yScale(d.value)));
+            .attr("d",function(d){
+                return d3.line()
+                    .x(d => xScale(d.date))
+                    .y(d => yScale(+d.value))
+                    (d[1])
+            })
+        container_g.selectAll("legend")
+            .data(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#1e90ff'])
+            .enter().append("circle")
+            .attr("class","circle")
+            .attr("cx",25)
+            .attr("cy",function(d,i){return 490 + i*25})
+            .attr("r",10)
+            .style("fill",function(d){ return d})
 
+        container_g.selectAll("labels")
+            .data(["Miss Saigon","Broken Glass","The Sisters Rosenweig","Beauty And The Beast","A Streetcar Named Desire 92","A Little More Magic"])
+            .enter()
+            .append("text")
+            .attr("font-family","sans-serif")
+            .attr("x",40)
+            .attr("y",function(d,i){return 500 + i*25})
+            .text(function(d){return d});
     })
 }
 main();
